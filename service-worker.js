90c856eea2f1c65f753e5023ba3445323a8177bd
similae <hola@similae.com>
2026-07-31 08:14:59 -0400

    Replace speed pickup with pixel tomato

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "pizza-invaders-v1.0.37";
+const CACHE_VERSION = "pizza-invaders-v1.0.38";
 const CORE_CACHE = `${CACHE_VERSION}-core`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
