eecd2139ff5d5e953769c1eb509b478ca3fadf14
similae <hola@similae.com>
2026-07-31 07:24:19 -0400

    Use the sound toggle tone for all effects

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "pizza-invaders-v1.0.31";
+const CACHE_VERSION = "pizza-invaders-v1.0.32";
 const CORE_CACHE = `${CACHE_VERSION}-core`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
@@ -17,11 +17,7 @@ const CORE_FILES = [
   "./assets/PIZZAINVADERS-POPOCOLA-INTRO-MOBILE.png",
   "./assets/PIZZAINVADERS-FROZENPIZZA-VIDEO-MOBILE.mp4",
   "./assets/PIZZAINVADERS-LOGO-SIMILAE-LOADING.png",
-  "./assets/sfx-confirm.wav",
-  "./assets/sfx-powerup.wav",
-  "./assets/sfx-hurt.wav",
-  "./assets/sfx-boss.wav",
-  "./assets/sfx-boss-down.wav"
+  "./assets/sfx-confirm.wav"
 ];
 
 self.addEventListener("install", event => {
