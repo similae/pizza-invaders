a1cfd9cfa96d3aa920efe5cb5322c19656d4463f
similae <hola@similae.com>
2026-07-30 23:06:58 -0400

    Remove boss names from combat

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "pizza-invaders-v1.0.15";
+const CACHE_VERSION = "pizza-invaders-v1.0.16";
 const CORE_CACHE = `${CACHE_VERSION}-core`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
