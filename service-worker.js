01c49c4c75bb6c7a8246435bd028c432830d1155
similae <hola@similae.com>
2026-07-31 12:31:10 -0400

    Improve water and basil pickup clarity

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "pizza-invaders-v1.0.43";
+const CACHE_VERSION = "pizza-invaders-v1.0.44";
 const CORE_CACHE = `${CACHE_VERSION}-core`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
