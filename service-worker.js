dca15853aeb33c76e8b9d12fd1bb1e0f4c084ebf
similae <hola@similae.com>
2026-07-31 12:15:47 -0400

    Redesign powerups and update intro guide

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "pizza-invaders-v1.0.41";
+const CACHE_VERSION = "pizza-invaders-v1.0.42";
 const CORE_CACHE = `${CACHE_VERSION}-core`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
