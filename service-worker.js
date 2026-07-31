e30a3a641100f4877fce0e3d9b1bd0e7d4e0a3c0
similae <hola@similae.com>
2026-07-31 13:30:07 -0400

    Restore detailed tomato and lengthen basil

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "pizza-invaders-v1.0.44";
+const CACHE_VERSION = "pizza-invaders-v1.0.45";
 const CORE_CACHE = `${CACHE_VERSION}-core`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
