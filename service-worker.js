ab3f77013c48c46509bcd59deb667a61b21edda6
similae <hola@similae.com>
2026-07-31 21:58:05 -0400

    Unify Pop Cola background across viewport

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "pizza-invaders-v1.0.57";
+const CACHE_VERSION = "pizza-invaders-v1.0.58";
 const CORE_CACHE = `${CACHE_VERSION}-core`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
