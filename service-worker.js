93b5230b17ad97797243d08690f00f4d9f9b26ff
similae <hola@similae.com>
2026-08-01 00:03:17 -0400

    Remove Freezer Machine desktop game border

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "pizza-invaders-v1.0.60";
+const CACHE_VERSION = "pizza-invaders-v1.0.61";
 const CORE_CACHE = `${CACHE_VERSION}-core`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
