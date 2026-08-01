f2eb8cb23195db693ad5d1b735b9a5e8d1ec8c95
similae <hola@similae.com>
2026-08-01 11:17:40 -0400

    Add Blockverse stage after Pineapple Panic

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "pizza-invaders-v1.0.78";
+const CACHE_VERSION = "pizza-invaders-v1.0.79";
 const CORE_CACHE = `${CACHE_VERSION}-core`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
