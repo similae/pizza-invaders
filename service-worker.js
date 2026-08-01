3cf67e1d78112b3577957cebed3e0f837115e603
similae <hola@similae.com>
2026-08-01 00:43:23 -0400

    Remove stage canvas frame at every desktop width

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "pizza-invaders-v1.0.67";
+const CACHE_VERSION = "pizza-invaders-v1.0.68";
 const CORE_CACHE = `${CACHE_VERSION}-core`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
