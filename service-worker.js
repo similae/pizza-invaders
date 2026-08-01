d7dd3f049c8af62580be78829d2b6264972a2b79
similae <hola@similae.com>
2026-08-01 12:10:52 -0400

    Unify Pernigotti prize transition background

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "pizza-invaders-v1.0.81";
+const CACHE_VERSION = "pizza-invaders-v1.0.82";
 const CORE_CACHE = `${CACHE_VERSION}-core`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
