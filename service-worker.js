8d147f513b3c9cf188e73c6e747aba79cdf78cc8
similae <hola@similae.com>
2026-08-01 01:00:28 -0400

    Expand hangar grid across viewport

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "pizza-invaders-v1.0.70";
+const CACHE_VERSION = "pizza-invaders-v1.0.71";
 const CORE_CACHE = `${CACHE_VERSION}-core`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
