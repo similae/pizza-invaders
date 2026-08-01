435b49f25674ef1d23939ded9a9d78fd0ea6f090
similae <hola@similae.com>
2026-08-01 00:15:14 -0400

    Unify Candy Factory background across viewport

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "pizza-invaders-v1.0.63";
+const CACHE_VERSION = "pizza-invaders-v1.0.64";
 const CORE_CACHE = `${CACHE_VERSION}-core`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
