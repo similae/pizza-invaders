56f281e42e9c9c7819e7ed89fb524c6bc9f9f8e6
similae <hola@similae.com>
2026-08-01 11:43:21 -0400

    Add Fungi Kingdom platformer stage

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "pizza-invaders-v1.0.80";
+const CACHE_VERSION = "pizza-invaders-v1.0.81";
 const CORE_CACHE = `${CACHE_VERSION}-core`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
