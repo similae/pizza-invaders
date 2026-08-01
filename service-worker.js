35f7eaa5bc2944f998cc246c5e306bf247e7dd59
similae <hola@similae.com>
2026-08-01 00:57:23 -0400

    Unify start screen space background

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "pizza-invaders-v1.0.69";
+const CACHE_VERSION = "pizza-invaders-v1.0.70";
 const CORE_CACHE = `${CACHE_VERSION}-core`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
