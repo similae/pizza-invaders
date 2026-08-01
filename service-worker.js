10a7e3a217c375b30a17b3d84e9560eb7b159a44
similae <hola@similae.com>
2026-07-31 23:53:36 -0400

    Extend Freezer Machine background across viewport

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "pizza-invaders-v1.0.58";
+const CACHE_VERSION = "pizza-invaders-v1.0.59";
 const CORE_CACHE = `${CACHE_VERSION}-core`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
